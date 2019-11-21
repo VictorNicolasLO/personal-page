2c8955c10193be5e1edf02098b71416da885e490
VictorNicolasLO <victornicolaslo@hotmail.com>
2019-11-21 00:29:57 -0700

    loaders

--- src/providers/app-status.hook.js
+++ src/providers/app-status.hook.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import MainBackground from '../assets/all-images/landscape-in-mountains.png';
+import Profile from '../assets/images/profile.png';
 function useAppStatus() {
-  const [ready, setReady] = React.useState();
+  const [ready, setReady] = React.useState(false);
   React.useEffect(() => {
     const img = new Image();
+    const imgProfile = new Image();
     img.src = MainBackground;
-    img.onload = () => {
-      setReady(true);
+    imgProfile.src = Profile;
+    let counter = 0;
+    function checkReady() {
+      counter++;
+      if (counter >= 2) setReady(true);
+    }
+    imgProfile.onload = () => {
+      checkReady();
     };
+    img.onload = () => {
+      checkReady();
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  alert(ready);
   return {
     ready,
   };
--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import Playground from './routes/playground';
 import Main from './routes/main';
 import { useAppStatusProvider } from './providers/app-status.provider';
 import { useThemeProvider } from './providers/theme.provider';
+import { BarLoader } from 'react-spinners';
+
 function MainRouter() {
   const { ready } = useAppStatusProvider();
   const {
@@ -11,18 +13,25 @@ function MainRouter() {
   } = useThemeProvider();
   if (!ready) {
     return (
-      <div
-        style={{
-          width: '100vw',
-          height: '100vh',
-          left: 0,
-          top: 0,
-          margin: 'auto',
-          bottom: 0,
-          right: 0,
-          background: secondary,
-        }}
-      />
+      <div>
+        <div
+          style={{
+            width: '100vw',
+            height: '100vh',
+            left: 0,
+            top: 0,
+            position: 'fixed',
+            margin: 'auto',
+            bottom: 0,
+            right: 0,
+            background: secondary,
+          }}
+        />
+        <BarLoader
+          css="position:absolute;top:0;margin:auto;width:100%"
+          color="white"
+        />
+      </div>
     );
   }
   return (
