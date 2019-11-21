008a317a9d7e8418ab28b4ba6027b676494bc046
VictorNicolasLO <victornicolaslo@hotmail.com>
2019-11-20 20:33:38 -0700

    disable eslint

--- src/routes/main/containers/contact-section/hooks/useContactForm.js
+++ src/routes/main/containers/contact-section/hooks/useContactForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 import useTextField from '../../../../../components/text-field/hooks/usetTextField';
 import { useContactsProvider } from '../../../providers/contact.provider';
 
